90940c90bba2bdfa3af8604c50b6ade3216cf56b
anshch005 <anshchaudhary655@gmail.com>
2026-04-06 23:06:23 +0530

    Rotation addition

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
-    
+    [SerializeField] InputAction Rotation;
     [SerializeField] float FlightForce;
 
     Rigidbody rb;
@@ -18,15 +18,30 @@ public class Movement : MonoBehaviour
     void OnEnable()
     {
         thrust.Enable();
+        Rotation.Enable();
     }
 
     private void FixedUpdate() {
+        {
+            UpwardForce();
+            RotationalForce();
+        }
+    }
+
+    private void UpwardForce()
     {
         if (thrust.IsPressed())
         {
-            rb.AddRelativeForce(Vector3.up* FlightForce* Time.fixedDeltaTime);
+            rb.AddRelativeForce(Vector3.up * FlightForce * Time.fixedDeltaTime);
             Debug.Log("wingardium leviosa");
         }
     }
+
+    private void RotationalForce()
+    {
+        float rotationInput = Rotation.ReadValue<float>();
+        Debug.Log("here is our rotation value: " + rotationInput);
     }
+
+
 }
