fe453eb7409299ec0f4cc7b5016724e85758992c
anshch005 <anshchaudhary655@gmail.com>
2026-04-26 17:01:44 +0530

    Delay system added and level layout changed for second level

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -1,42 +1,53 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CollisionDetect : MonoBehaviour
 {
-     private void OnCollisionEnter(Collision other) 
-    {  
+    [SerializeField] float LevelLoadDelay = 2f;
+    private void OnCollisionEnter(Collision other) 
+{  
+    {
+    switch(other.gameObject.tag)
         {
-        switch(other.gameObject.tag)
-        {
-            case "Start":
-                Debug.Log("Start pull the trigger");
-                break;
-            case "Finish":
-                LevelUp();
-                break;
-            default:
-                ReloadLevel();
-                break;
+        case "Start":
+            Debug.Log("Start pull the trigger");
+            break;
+        case "Finish":
+            StartSuccessSequence();
+            break;
+        default:
+            StartCrashSequence();
+            break;
         }
-        }
-        void ReloadLevel()
-        {
-            int currentScene = SceneManager.GetActiveScene().buildIndex;
-            int nextscene =  currentScene + 1;
-            
-            if (nextscene == SceneManager.sceneCountInBuildSettings)
-            {
-                nextscene= 0;
-            }
+    }
+}
+    void StartSuccessSequence()
+    {
+        GetComponent<Movement>().enabled=false;
+        Invoke("LoadNextLevel",LevelLoadDelay);//minor delay before loading the level
+    }
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled=false; //turns off the movement during that window 
+        Invoke("ReloadLevel",LevelLoadDelay); //invoke for delay 
+    }
+    void LoadNextLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextscene =  currentScene + 1;
 
-            SceneManager.LoadScene(nextscene);
-        }
-
-        void LevelUp()
+        if (nextscene == SceneManager.sceneCountInBuildSettings)
         {
-            int currentScene = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(currentScene + 1);
+            nextscene= 0;
         }
+        SceneManager.LoadScene(nextscene);
+    }
+    void ReloadLevel()
+    {
+
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
     }
 }
